3962609a6a45c827d4099ce1ec62f00a51f4c82e
Dawitdkt <dawitkinfu5@gmail.com>
2023-02-25 00:03:53 +0300

    small fix on access_nested_map

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -10,7 +10,10 @@ from utils import access_nested_map
 
 class TestAccessNestedMap(unittest.TestCase):
     
-    @ParameterizedMIMEHeader.expand([({"a": 1},("a",),1),({"a": {"b": 2}},("a",),{'b': 2}),{"a": {"b": 2}}, ("a", "b"),2])
+    @ParameterizedMIMEHeader.expand([
+        ({"a": 1},("a",),1),
+        ({"a": {"b": 2}},("a",),{'b': 2}),
+        {"a": {"b": 2}},("a", "b"),2])
     def test_access_nested_map(self):
         """test the access_nested_map method of utils"""
-        self.assertTrue(access_nested_map(nested_map={"a": 1}, path=("a",)))
+        self.assertEqual(access_nested_map(nested_map={"a": 1}, path=("a",)))
