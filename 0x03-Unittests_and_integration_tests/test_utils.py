65e523585040dc6ef869a7ad0ce380bb69d0709f
Dawitdkt <dawitkinfu5@gmail.com>
2023-02-25 00:29:22 +0300

    all done

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
 
 """doc goes here"""
-
-from email.headerregistry import ParameterizedMIMEHeader
 import unittest
-
+from parameterized import parameterized
 from utils import access_nested_map
+from typing import (
+    Mapping,
+    Sequence,
+    Any,
+    Dict,
+    Callable,
+)
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    @ParameterizedMIMEHeader.expand([
+    @parameterized.expand([
         ({"a": 1}, ("a", ), 1),
         ({"a": {"b": 2}}, ("a", ), {'b': 2}),
-        {"a": {"b": 2}}, ("a", "b"), 2])
-    def test_access_nested_map(self):
+        ({"a": {"b": 2}}, ("a", "b"), 2)])
+    def test_access_nested_map(self,nested_map: Mapping, path: Sequence, expected:any):
         """test the access_nested_map method of utils"""
-        self.assertEqual(access_nested_map(nested_map={"a": 1}, path=("a",)))
+        self.assertEqual(access_nested_map(nested_map, path),expected)
