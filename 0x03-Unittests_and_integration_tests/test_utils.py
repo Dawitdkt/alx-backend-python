48124084bf44f4625e4d578172658bcc35820885
Dawitdkt <dawitkinfu5@gmail.com>
2023-02-25 00:08:12 +0300

    linter fix

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -9,11 +9,10 @@ from utils import access_nested_map
 
 
 class TestAccessNestedMap(unittest.TestCase):
-    
     @ParameterizedMIMEHeader.expand([
-        ({"a": 1},("a",),1),
-        ({"a": {"b": 2}},("a",),{'b': 2}),
-        {"a": {"b": 2}},("a", "b"),2])
+        ({"a": 1}, ("a", ), 1),
+        ({"a": {"b": 2}}, ("a", ), {'b': 2}),
+        {"a": {"b": 2}}, ("a", "b"), 2])
     def test_access_nested_map(self):
         """test the access_nested_map method of utils"""
         self.assertEqual(access_nested_map(nested_map={"a": 1}, path=("a",)))
